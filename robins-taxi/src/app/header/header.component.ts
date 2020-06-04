fba36b32c07cf39733860159bad4bca1a6294a27
nuttygeek-arun <nuttygeek.arun@gmail.com>
2020-06-04 14:59:53 +0530

    website done content to be chnaged

--- robins-taxi/src/app/header/header.component.ts
+++ robins-taxi/src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, AfterViewInit{
+  
+  @ViewChild('mobile', {static: false}) mobileMenu: ElementRef;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    this.mobileMenu.nativeElement.style.display = "none";
+  }
+
+  menuClicked(){
+    this.mobileMenu.nativeElement.style.display = 'flex';
+
+  }
+
+  closeClicked(){
+    this.mobileMenu.nativeElement.style.display = "none";
   }
 
 }
