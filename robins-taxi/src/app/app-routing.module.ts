0d6b07375b1274c5e4af2d64aff8005fc9d639fd
mkumar08 <milankumar23@aol.com>
2023-10-18 15:08:25 -0400

    removed unneeded component, formatted code via Prettier, updated iframe styling to be more responsive/mobile-friendly

--- robins-taxi/src/app/app-routing.module.ts
+++ robins-taxi/src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
-
-const routes: Routes = [{
-  path: '', component: HomeComponent
-}];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- robins-taxi/src/app/footer/footer.component.ts
+++ robins-taxi/src/app/footer/footer.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
+  styleUrls: ["./footer.component.css"],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onNavigatetoTaxiReservation() {
+    const url =
+      "https://portal.pergorides.io/reservation/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYm5hbWUiOiJwZXJnbzMzOTY0Y2FyIiwiaWF0IjoxNjgwMjkxMjQ3fQ.uUxP9zfAul33_WUULS_iiK_mWqzr4hPkAfmcM02a3rQ&type=passenger";
+    window.open(url, "_blank");
   }
 
+  onNavigatetoMedicalReservation() {
+    const url =
+      "https://portal.pergorides.io/reservation/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYm5hbWUiOiJwZXJnbzMzOTY0Y2FyIiwiaWF0IjoxNjgwMjkxMjQ3fQ.uUxP9zfAul33_WUULS_iiK_mWqzr4hPkAfmcM02a3rQ&type=account";
+    window.open(url, "_blank");
+  }
 }
