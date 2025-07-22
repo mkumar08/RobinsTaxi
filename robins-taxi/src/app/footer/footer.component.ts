0fc50cfe8c4b2e6cb8574897ccc5fd30f20f8645
mkumar08 <milankumar23@aol.com>
2025-07-21 22:03:12 -0400

    removed iframe, pergo booking, updated copyright, removed unneeded code

--- robins-taxi/src/app/footer/footer.component.ts
+++ robins-taxi/src/app/footer/footer.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-footer",
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.css"],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent {
 
-  ngOnInit() {}
-
-  onNavigatetoTaxiReservation() {
-    const url =
-      "https://portal.pergorides.io/reservation/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYm5hbWUiOiJwZXJnbzMzOTY0Y2FyIiwiaWF0IjoxNjgwMjkxMjQ3fQ.uUxP9zfAul33_WUULS_iiK_mWqzr4hPkAfmcM02a3rQ&type=passenger";
-    window.open(url, "_blank");
-  }
+  // onNavigatetoTaxiReservation() {
+  //   const url =
+  //     "https://portal.pergorides.io/reservation/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYm5hbWUiOiJwZXJnbzMzOTY0Y2FyIiwiaWF0IjoxNjgwMjkxMjQ3fQ.uUxP9zfAul33_WUULS_iiK_mWqzr4hPkAfmcM02a3rQ&type=passenger";
+  //   window.open(url, "_blank");
+  // }
 
   onNavigatetoMedicalReservation() {
     const url =
-      "https://portal.pergorides.io/reservation/?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYm5hbWUiOiJwZXJnbzMzOTY0Y2FyIiwiaWF0IjoxNjgwMjkxMjQ3fQ.uUxP9zfAul33_WUULS_iiK_mWqzr4hPkAfmcM02a3rQ&type=account";
+      "https://www.medanswering.com/";
     window.open(url, "_blank");
   }
 }
--- robins-taxi/src/app/header/header.component.ts
+++ robins-taxi/src/app/header/header.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, AfterViewInit{
-  
-  @ViewChild('mobile', {static: false}) mobileMenu: ElementRef;
-  
-  constructor() { }
+export class HeaderComponent implements AfterViewInit{
 
-  ngOnInit() {}
+  @ViewChild('mobile', {static: false}) mobileMenu: ElementRef;
 
   ngAfterViewInit(){
     this.mobileMenu.nativeElement.style.display = "none";
