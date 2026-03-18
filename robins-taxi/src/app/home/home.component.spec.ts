2c1bbbbf399e9dbc3aba05db084913ef66839732
mkumar08 <milankumar23@aol.com>
2026-03-18 14:20:10 -0400

    chore: upgrade Angular to v21 with modernization
    - Framework: Angular 8 to 21 with all major-version migrations applied
    - Bootstrap: from NgModule platformBrowserDynamic to standalone bootstrapApplication
    - Routing: from NgModule RouterModule to standalone routes with provideRouter
    - Components: all marked standalone with explicit imports, modules removed
    - Linting: ESLint with angular-eslint flat config (replaced TSLint)
    - E2E: Playwright with config and smoke tests (replaced Protractor)
    - Testing: async() to waitForAsync(), standalone test specs
    - Dependencies: RxJS, TypeScript, zone.js modernized
    - Validation: lint 0 errors, build 2.48MB, 6 tests passing

--- robins-taxi/src/app/home/home.component.spec.ts
+++ robins-taxi/src/app/home/home.component.spec.ts
@@ -1,14 +1,14 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { HomeComponent } from "./home.component";
+import { HomeComponent } from './home.component';
 
 describe("HomeComponent", () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      imports: [HomeComponent],
     }).compileComponents();
   }));
 
